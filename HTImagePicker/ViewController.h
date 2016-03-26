37d16c38f47a6a3df07eb54d70ee28f797fe6fef
孙大圣 <sclcoder@163.com>
2016-03-26 16:12:48 +0800

    Initial Commit

---
+++ HTImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTImagePicker
+//
+//  Created by sunchunlei on 16/3/26.
+//  Copyright © 2016年 godHands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
